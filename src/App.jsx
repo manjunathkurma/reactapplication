44b15bb28c388e8f58f8f64bf0c9f397a9a1b12c
manjunathkurma <139434507+manjunathkurma@users.noreply.github.com>
2023-09-12 15:34:38 +0530

    fdgh

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import Sformik from "./Sformik";
-function App() {
-  return (
+import Counter from "./Counter";
+import store from "./store/store";
+import Todolist from "./Todolist";
+import { Provider } from "react-redux";
+function App(){
+  return(
+    <Provider store={store}>
     <div className="box">
-      <h1>Students</h1>
-      <Sformik></Sformik>
+      <h1>Welcome to ReactJS Training</h1>
+      <Counter></Counter>
+      <Todolist></Todolist>
     </div>
-  );
+    </Provider>
+  )
 }
-
 export default App;
